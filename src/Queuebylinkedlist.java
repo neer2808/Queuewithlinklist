b2a362f5355ecc48f20bee7c939940f312d8d46d
neer2808 <uic.neerajkhanna@gmail.com>
2020-02-14 07:08:53 +0530

    Initial commit

---
+++ src/Queuebylinkedlist.java
@@ -0,0 +1,23 @@
+public class Queuebylinkedlist {
+  Node front;
+  Node rear;
+
+  public Queuebylinkedlist() {
+    front= null;
+    rear = null;
+  }
+
+ public void enqueue(Node newnode)
+ {
+
+ }
+
+ public void dequeue()
+ {
+
+ }
+ public void printqueue()
+ {
+   
+ }
+}
