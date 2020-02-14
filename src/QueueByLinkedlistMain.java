4b2409df297bc86d00499685cbe10495a5a94622
neer2808 <uic.neerajkhanna@gmail.com>
2020-02-14 07:18:03 +0530

    add method definitions

--- src/QueueByLinkedlistMain.java
+++ src/QueueByLinkedlistMain.java
@@ -1,2 +1,12 @@
 public class QueueByLinkedlistMain {
+  public static void main(String[] args) {
+    Queuebylinkedlist obj = new Queuebylinkedlist();
+
+    Node n1 = new Node(10);
+    obj.enqueue(n1);
+    Node n2 = new Node(20);
+    obj.enqueue(n2);
+    obj.printqueue();
+
+  }
 }
--- src/Queuebylinkedlist.java
+++ src/Queuebylinkedlist.java
@@ -6,18 +6,31 @@ public class Queuebylinkedlist {
     front= null;
     rear = null;
   }
-
  public void enqueue(Node newnode)
  {
-
+  if(front==null && rear ==null)
+  {
+    front=rear=newnode;
+  }
+  else
+  {
+    rear.next= newnode;
+    rear= newnode;
+  }
  }
-
  public void dequeue()
  {
+
 
  }
  public void printqueue()
  {
-   
+   Node temp = front;
+   while(temp!= null)
+   {
+     System.out.println(temp.data);
+     temp = temp.next;
+   }
  }
+
 }
